3fe08a99779655eb63f5f03880064a3773be9bf1
Kovrizhnykh Nikolai <kWX792282@china.huawei.com>
2020-04-14 20:23:45 +0300

    S1T3 output requirements

--- S1T3_newton_method/py/newton_method.py
+++ S1T3_newton_method/py/newton_method.py
@@ -8,6 +8,7 @@ def solve_scalar(f: sp.Expr, x0, tol):
     solve scalar equation y=f(x) starting with x0
     derivative can be obtained as f.diff()
     evaluate: float(f.subs(x, x0))
+    return list of x, list of y
     """
     raise NotImplementedError
 
@@ -16,5 +17,6 @@ def solve_plane(f: sp.Matrix, x0, y0, tol):
     """
     solve SAE {f1(x,y) = 0, f2(x,y) = 0} starting with (x0,y0)
     jacobian can be obtained as f.jacobian([x, y])
+    return list of x, list of y, list of np.linalg.norm(z)
     """
     raise NotImplementedError
